819fd9194683e0d54619357caada94776863d48d
Alan Murphy <almurphy101@hotmail.com>
2022-09-23 13:39:19 +0100

    Simple Algo Upload

---
+++ Palindrome/src/userName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class userName{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name");
+            String strng = scanner.nextLine();
+            System.out.println(strng);
+            
+    }
+}
